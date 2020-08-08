b25b806d4b643a6670e5032c8b7fcec8dfe59046
Leo Zhang <leoucfstudent@knights.ucf.edu>
2020-08-08 13:58:02 -0400

    text101

---
+++ Assets/State.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.AccessControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+  [TextArea(10,14)][SerializeField] string storyText;
+    [SerializeField] State[] states;
+
+    public State[] States { get {return states;} }
+
+// getters
+    [Obsolete]
+    public State[] NextStates()
+    {
+        return states;
+    }
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
